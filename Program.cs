14a929977aee7e0c6161934022adf1a8c31a106a
anuyadav2494 <54722794+anuyadav2494@users.noreply.github.com>
2020-01-21 20:22:01 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day_2_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to GitHub");
+            Console.ReadLine();
+        }
+    }
+}
